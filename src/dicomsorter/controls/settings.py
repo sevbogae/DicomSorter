e7bed3ad70ad1a5bef694472b7904bd609faccdd
Seppe Van Bogaert <seppe.van.bogaert@icloud.com>
2026-02-20 14:47:34 +0100

    Added TODO's for settings.
    Signed-off-by: Seppe Van Bogaert <seppe.van.bogaert@icloud.com>

--- src/dicomsorter/controls/settings.py
+++ src/dicomsorter/controls/settings.py
@@ -5,6 +5,11 @@ from toml import load, dump
 from dicomsorter.controls.explorer import get_asset
 
 
+# TODO: Change this to a more robust way of handling settings. Look into platformdirs or something similar.
+#  Because the _MEIPASS directory is actually read-only and resets every time the application is restarted, we
+#  need to save the settings to a different location.
+
+
 def read_settings() -> dict[str, Any]:
     """Read settings from a TOML file."""
     try:
@@ -17,8 +22,3 @@ def save_settings(settings: dict[str, Any]) -> None:
     """Save settings to a TOML file."""
     with open(get_asset("assets/settings.toml"), "w") as f:
         dump(settings, f)
-
-
-if __name__ == "__main__":
-    settings = read_settings()
-    print(settings)
